31fe04c702e74deaf1c6ddafe05c58773713078f
patflynn89 <patflynn89@users.noreply.github.com>
2020-01-31 23:23:14 +0100

    Create TriangleTester.java
    Test if you can produce a valid triangle with the given ints

---
+++ TriangleTester.java
@@ -0,0 +1,9 @@
+public class TriangleTester{
+  public static boolean isTriangle(int a, int b, int c){
+    if ((a+b)>c && (a+c)>b && (b+c)>a ) {
+      return true;
+    }
+    return false;
+    
+  }
+}
